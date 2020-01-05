104748adcd1c0a4b9ff6ade86c880def9dd6ada3
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-01-05 17:21:41 +0900

    トグルアニメーション作成

--- src/script/index.js
+++ src/script/index.js
@@ -11,9 +11,11 @@ class Toggle {
 
     this.isHover = false; // ホバー中かどうか
     this.isAnimation = false; // アニメーション中かどうか
+    this.isClick = false;
 
     this.onMouseenter = this.onMouseenter.bind(this);
     this.onMouseleave = this.onMouseleave.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   init () {
@@ -22,35 +24,31 @@ class Toggle {
   }
 
   onListener () {
+    this.toggle.addEventListener('click', this.onClick);
     this.toggle.addEventListener('mouseenter', this.onMouseenter);
     this.toggle.addEventListener('mouseleave', this.onMouseleave);
+  }
+
+  onClick () {
+    this.isClick = !this.isClick;
+
+    if (this.isClick) {
+      this.lineWraps.forEach(r => {
+        r.classList.add('open');
+      });
+    } else {
+      this.lineWraps.forEach(r => {
+        r.classList.remove('open');
+      });
+    }
+
+    this.animation();
   }
 
   onMouseenter () {
     if (!this.isHover) {
       this.isAnimation = true;
-      this.lines.forEach((el, i) => {
-        gsap.to(el, {
-          x: width,
-          duration: 0.3,
-          delay: i * 0.1,
-          ease: 'expo.out',
-          onComplete: () => {
-            gsap.set(el, {
-              x: -width
-            });
-
-            gsap.to(el, {
-              x: 0,
-              duration: 0.3,
-              ease: 'expo.out',
-              onComplete: () => {
-                this.isAnimation = false;
-              }
-            });
-          }
-        });
-      });
+      this.animation();
     }
 
     this.isHover = true;
@@ -58,10 +56,31 @@ class Toggle {
 
   onMouseleave () {
     this.isHover = false;
+  }
 
-    // this.lines.forEach(el => {
-    //   gsap.killTweensOf(el);
-    // });
+  animation () {
+    this.lines.forEach((el, i) => {
+      gsap.to(el, {
+        x: width,
+        duration: 0.3,
+        delay: i * 0.1,
+        ease: 'expo.out',
+        onComplete: () => {
+          gsap.set(el, {
+            x: -width
+          });
+
+          gsap.to(el, {
+            x: 0,
+            duration: 0.3,
+            ease: 'expo.out',
+            onComplete: () => {
+              this.isAnimation = false;
+            }
+          });
+        }
+      });
+    });
   }
 }
 
